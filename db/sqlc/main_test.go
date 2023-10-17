26bad0b16406aec73179edd85035b2c754d5b8eb
timly <timly278@gmail.com>
2023-10-17 17:07:08 +0700

    add loading config using Viber

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -5,26 +5,27 @@ import (
 	"log"
 	"os"
 	"testing"
-	_ "github.com/lib/pq"
-)
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:tulb@localhost:5432/simple_bank?sslmode=disable"
+	_ "github.com/lib/pq"
+	"github.com/timly278/simplebank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-
 func TestMain(m *testing.M) {
-	db, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../../")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	db, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
 	testDB = db
-	testQueries = New(db) 
-	
+	testQueries = New(db)
+
 	os.Exit(m.Run())
 }
--- main.go
+++ main.go
@@ -7,23 +7,25 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/timly278/simplebank/api"
 	db "github.com/timly278/simplebank/db/sqlc"
+	"github.com/timly278/simplebank/util"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:tulb@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main() {
-	dbconn, err := sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	dbconn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(dbconn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
