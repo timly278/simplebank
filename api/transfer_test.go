a69f076f14eb8733e9b023f1ccf59d64ad08e7e8
timly <timly278@gmail.com>
2023-10-19 09:40:15 +0700

    improve TestTransferAPI, not yet fulfill testcases

--- api/transfer_test.go
+++ api/transfer_test.go
@@ -3,11 +3,11 @@ package api
 import (
 	"bytes"
 	"encoding/json"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 	mockdb "github.com/timly278/simplebank/db/mock"
@@ -16,112 +16,64 @@ import (
 )
 
 func TestTransferAPI(t *testing.T) {
-	transferParams, transferResults := randomTransfer()
-
-	ctrl := gomock.NewController(t)
-	store := mockdb.NewMockStore(ctrl)
-
-	store.EXPECT().
-		TransferTx(gomock.Any(), gomock.Eq(transferParams)).
-		Times(1).
-		Return(transferResults, nil)
-
-	FromAccount := db.Account{
-		ID:       transferParams.FromAccountID,
-		Owner:    transferResults.FromAccount.Owner,
-		Balance:  transferResults.FromAccount.Balance + transferParams.Amount,
-		Currency: transferResults.FromAccount.Currency,
-	}
-	store.EXPECT().
-		GetAccount(gomock.Any(), gomock.Eq(transferParams.FromAccountID)).
-		Times(1).
-		Return(FromAccount, nil)
-
-	ToAccount := db.Account{
-		ID:       transferParams.ToAccountID,
-		Owner:    transferResults.ToAccount.Owner,
-		Balance:  transferResults.ToAccount.Balance - transferParams.Amount,
-		Currency: transferResults.ToAccount.Currency,
-	}
-	store.EXPECT().
-		GetAccount(gomock.Any(), gomock.Eq(transferParams.ToAccountID)).
-		Times(1).
-		Return(ToAccount, nil)
-
-	// start test server and send request
-	server := NewServer(store)
-	recorder := httptest.NewRecorder()
-
-	transferRequest := createTransferRequest{
-		FromAccountID: transferParams.FromAccountID,
-		ToAccountID:   transferParams.ToAccountID,
-		Amount:        transferParams.Amount,
-		Currency:      transferResults.FromAccount.Currency,
-	}
-	body, err := json.Marshal(transferRequest)
-	require.NoError(t, err)
-
-	url := "/transfers"
-	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
-	require.NoError(t, err)
-
-	server.router.ServeHTTP(recorder, request)
-	requireBodyMatchTransfer(t, recorder.Body, transferResults)
-}
-
-// randomTransfer
-func randomTransfer() (transferPrams db.TransferTxPrams, transferResults db.TransferTxResults) {
+	amount := int64(10)
 	account1 := randomAccount()
 	account2 := randomAccount()
-	account1.Currency = account2.Currency
 
-	transferPrams = db.TransferTxPrams{
-		FromAccountID: account1.ID,
-		ToAccountID:   account2.ID,
-		Amount:        10,
-	}
+	account1.Currency = util.USD
+	account2.Currency = util.USD
 
-	transferResults = db.TransferTxResults{
-		Transfer: db.Transfer{
-			ID:            util.RandomInt(1, 1000),
-			FromAccountID: transferPrams.FromAccountID,
-			ToAccountID:   transferPrams.ToAccountID,
-			Amount:        transferPrams.Amount,
-		},
-		FromAccount: db.Account{
-			ID:       transferPrams.FromAccountID,
-			Owner:    account1.Owner,
-			Balance:  account1.Balance - transferPrams.Amount,
-			Currency: account1.Currency,
-		},
-		ToAccount: db.Account{
-			ID:       transferPrams.ToAccountID,
-			Owner:    account2.Owner,
-			Balance:  account2.Balance + transferPrams.Amount,
-			Currency: account2.Currency,
-		},
-		FromEntry: db.Entry{
-			ID:        util.RandomInt(1, 1000),
-			AccountID: transferPrams.FromAccountID,
-			Amount:    -transferPrams.Amount,
-		},
-		ToEntry: db.Entry{
-			ID:        transferResults.FromEntry.ID + 1,
-			AccountID: transferPrams.ToAccountID,
-			Amount:    transferPrams.Amount,
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "Happy Case",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        account1.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account1.ID)).Times(1).Return(account1, nil)
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account2.ID)).Times(1).Return(account2, nil)
+
+				transferParams := db.TransferTxPrams{
+					FromAccountID: account1.ID,
+					ToAccountID:   account2.ID,
+					Amount:        amount,
+				}
+				store.EXPECT().TransferTx(gomock.Any(), gomock.Eq(transferParams)).Times(1)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
 		},
 	}
 
-	return transferPrams, transferResults
-}
+	for _, tc := range testCases {
 
-func requireBodyMatchTransfer(t *testing.T, body *bytes.Buffer, transferResults db.TransferTxResults) {
-	data, err := io.ReadAll(body)
-	require.NoError(t, err)
+		ctrl := gomock.NewController(t)
+		store := mockdb.NewMockStore(ctrl)
 
-	var gotTransfer db.TransferTxResults
+		tc.buildStubs(store)
 
-	err = json.Unmarshal(data, &gotTransfer)
-	require.Equal(t, transferResults, gotTransfer)
-	require.NoError(t, err)
+		// start test server and send request
+		server := NewServer(store)
+		recorder := httptest.NewRecorder()
+
+		data, err := json.Marshal(tc.body)
+		require.NoError(t, err)
+
+		url := "/transfers"
+		request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
+		require.NoError(t, err)
+
+		server.router.ServeHTTP(recorder, request)
+
+		tc.checkResponse(t, recorder)
+	}
 }
